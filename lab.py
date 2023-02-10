d35547999a7e8f095b0f93a26d4e59317e2fc45d
oINCLUDEo <77641549+oINCLUDEo@users.noreply.github.com>
2023-02-10 14:05:36 +0400

    Add files via upload

--- lab.py
+++ lab.py
@@ -16,7 +16,7 @@ with open('nums.txt') as file:
     a = file.readline().split()
 for i in a:
     h = str(i)
-    if h.isdigit() and int(h) > 1:
+    if h.isdigit() and int(h) >= 1:
         if int(h[0]) % 2 != 0 and int(h) % 2 == 0:
             c.append(h)
 for i in c:
