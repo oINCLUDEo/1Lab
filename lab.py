19865535b6959ca5166d3887b04b8807c0c35976
oINCLUDEo <77641549+oINCLUDEo@users.noreply.github.com>
2023-02-17 10:45:52 +0400

    Add files via upload

--- lab.py
+++ lab.py
@@ -1,4 +1,7 @@
-c = []
+max_buffer_len = 100
+buffer_len = 1
+work_buffer = ''
+work_buffer_len = buffer_len
 num1 = {1: 'один', 2: 'два', 3: 'три', 4: 'четыре', 5: 'пять',
         6: 'шесть', 7: 'семь', 8: 'восемь', 9: 'девять'}
 num2 = {1: 'десять', 2: 'двадцать', 3: 'тридцать', 4: 'сорок',
@@ -13,40 +16,48 @@ num4 = {1: 'сто', 2: 'двести', 3: 'триста', 4: 'четырест�
 num5 = {1: 'одна тысяча', 2: 'две тысячи', 3: 'три тысячи', 4: 'четыре тысячи',
         5: 'пять тысяч', 6: 'шесть тысяч', 7: 'семь тысяч', 8: 'восемь тысяч', 9: 'девять тысяч'}
 with open('nums.txt') as file:
-    a = file.readline().split()
-for i in a:
-    h = str(i)
-    if h.isdigit() and int(h) >= 1:
-        if int(h[0]) % 2 != 0 and int(h) % 2 == 0:
-            c.append(h)
-for i in c:
-    d = len(i)
-    count = 0
-    if d == 4:
-        if i[count] != '0':
-            print(num5[int(i[count])], end=' ')
-        count += 1
-        d -= 1
-    if d == 3:
-        if i[count] != '0':
-            print(num4[int(i[count])], end=' ')
-        count += 1
-        d -= 1
-    if d == 2:
-        if i[count] != '0':
-            if int(i[count]) == 1:
-                print(num3[int(i[count:])], end=' ')
-                d -= 2
-                count += 2
-            else:
-                print(num2[int(i[count])], end=' ')
-                d -= 1
-                count += 1
-        else:
-            d -= 1
-            count += 1
-    if d == 1:
-        if i[count] != '0':
-            print(num1[int(i[count])], end=' ')
-        d -= 1
-    print()
+    a = file.read(buffer_len)
+    if not a:
+        print('\n Файл в директории проекта пустой')
+    while a:
+        while '0' <= a <= '9':
+            if '0' <= a <= '9':
+                work_buffer += a
+            a = file.read(buffer_len)
+        if len(work_buffer) > 0:
+            i = work_buffer
+            h = str(i)
+            if h.isdigit() and int(h) >= 1:
+                if int(h[0]) % 2 != 0 and int(h) % 2 == 0:
+                    d = len(i)
+                    count = 0
+                    if d == 4:
+                        if i[count] != '0':
+                            print(num5[int(i[count])], end=' ')
+                        count += 1
+                        d -= 1
+                    if d == 3:
+                        if i[count] != '0':
+                            print(num4[int(i[count])], end=' ')
+                        count += 1
+                        d -= 1
+                    if d == 2:
+                        if i[count] != '0':
+                            if int(i[count]) == 1:
+                                print(num3[int(i[count:])], end=' ')
+                                d -= 2
+                                count += 2
+                            else:
+                                print(num2[int(i[count])], end=' ')
+                                d -= 1
+                                count += 1
+                        else:
+                            d -= 1
+                            count += 1
+                    if d == 1:
+                        if i[count] != '0':
+                            print(num1[int(i[count])], end=' ')
+                        d -= 1
+                    print()
+        work_buffer = ''
+        a = file.read(buffer_len)
